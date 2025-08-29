f9a01ac4ba53151e0227848dd554ea82add19655
jbannisterly <jbannisterly@gmail.com>
2025-08-29 12:22:21 +0100

    webgl init

--- Frontend/game.js
+++ Frontend/game.js
@@ -1,21 +1,43 @@
 const canvas = document.getElementById("can");
-const gl = canvas.getContext("webgl");
+/** @type {WebGLRenderingContext} */
+const gl = canvas.getContext("webgl2");
 
-const vert_simple = CompileShader(VERTEX_SHADER_SIMPLE, gl.VERTEX_SHADER, gl);
-const frag_simple = CompileShader(FRAGMENT_SHADER_SIMPLE, gl.FRAGMENT_SHADER, gl);
-
-var program = gl.createProgram();
-gl.attachShader(program, vert_simple);
-gl.attachShader(program, frag_simple);
-gl.linkProgram(program);
-
-
-gl.clearColor(1,0,0,1);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-requestAnimationFrame(MainLoop);
 
 var startTime = -1;
+var program;
+var vertexBuffer;
+var vao;
+
+function Init(){
+  const vert_simple = CompileShader(VERTEX_SHADER_SIMPLE, gl.VERTEX_SHADER, gl);
+  const frag_simple = CompileShader(FRAGMENT_SHADER_SIMPLE, gl.FRAGMENT_SHADER, gl);
+
+  program = gl.createProgram();
+  gl.attachShader(program, vert_simple);
+  gl.attachShader(program, frag_simple);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+
+  var vertexPos = [
+    0., 0.,
+    0.5, 0.,
+    0.5, 0.5,
+  ]
+
+  vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexPos), gl.STATIC_DRAW);
+
+  vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+
+  var loc = gl.getAttribLocation(program, "position");
+  console.log(loc);
+  gl.enableVertexAttribArray(loc);
+  gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
+
+  requestAnimationFrame(MainLoop);
+}
 
 function UpdateGame(){
 
@@ -23,6 +45,14 @@ function UpdateGame(){
 
 function UpdateDisplay(){
 
+  vertexPos()
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexPos), gl.STATIC_DRAW);
+
+
+  gl.clearColor(1,0,0,1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
 
 function MainLoop(currentTime){
@@ -39,25 +69,28 @@ function CompileShader(source, type, gl){
     var shader = gl.createShader(type);
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
+    console.log(gl.getShaderInfoLog(shader));
     return shader;
 }
 
 const VERTEX_SHADER_SIMPLE = 
-`
-#version 300 es
+`#version 300 es
 
-in vec4 position;
+precision highp float;
+
+in vec2 position;
 out vec4 colour;
 
 void main(){
   colour = vec4(0., 0., 1., 1.);
-  gl_Position = position;
+  gl_Position = vec4(position.x, position.y, 1., 1.);
 }
 `;
 
 const FRAGMENT_SHADER_SIMPLE = 
-`
-#version 300 es
+`#version 300 es
+
+precision highp float;
 
 in vec4 colour;
 out vec4 fragColour;
@@ -66,3 +99,5 @@ void main(){
   fragColour = colour;
 }
 `
+
+Init();
